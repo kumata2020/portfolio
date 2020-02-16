930006d507ae5c8c09859fea1a04cb19443a3068
kumata2020 <kumata2020@gmail.com>
2020-02-16 20:12:52 +0900

    second commit

---
+++ js/scroll.js
@@ -0,0 +1,36 @@
+$('.to-home').click(function () {
+    $('body,html').animate({
+        scrollTop: $('#home').offset().top
+    }, 1000);
+    return false;
+});
+$('.to-work').click(function () {
+    $('body,html').animate({
+        scrollTop: $('#work').offset().top
+    }, 1000);
+    return false;
+});
+$('.to-features').click(function () {
+    $('body,html').animate({
+        scrollTop: $('#features').offset().top
+    }, 1000);
+    return false;
+});
+$('.to-resources').click(function () {
+    $('body,html').animate({
+        scrollTop: $('#resources').offset().top
+    }, 1000);
+    return false;
+});
+$('.to-clients').click(function () {
+    $('body,html').animate({
+        scrollTop: $('#clients').offset().top
+    }, 1000);
+    return false;
+});
+$('.to-contact').click(function () {
+    $('body,html').animate({
+        scrollTop: $('#contact').offset().top
+    }, 1000);
+    return false;
+});
